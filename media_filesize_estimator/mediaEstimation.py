b35dd738c8b3103b50454120e03eba634911977e
Harshit Kumar <kumar_harshit@outlook.com>
2022-08-10 09:15:28 +0530

    chore: Format code and add dependencies

--- media_filesize_estimator/mediaEstimation.py
+++ media_filesize_estimator/mediaEstimation.py
@@ -1,17 +1,17 @@
 # import packages which are necessary
 from fileinput import filename
-from msilib.schema import Error
 
 import matplotlib
 import pymediainfo
 from mediaExtraction import Extraction
+
 
 class Estimation:
     """
     import Extraction class and create an object if the object is not created already
     """
 
-    def __init__(self, mediaFilePath = None):
+    def __init__(self, mediaFilePath=None):
         """
         Initialise Extraction class to check if the given media file is valid or not
         """
@@ -21,19 +21,19 @@ class Estimation:
             mediainfoDictionary = mediainfoObject.to_data()
         except:
             print("An error occured, please check the file and try again")
-        
-        self.tracksData = mediainfoDictionary['tracks']
+
+        self.tracksData = mediainfoDictionary["tracks"]
         pass
 
-    def Estimate(self, mediaProperty = None):
+    def Estimate(self, mediaProperty=None):
         """
         param : Particular media property which needs to be estimated against
         return : Estimation sizes of the media file
         """
-        
+
         pass
 
-    def plotGraph(self, mediaProperty = None):
+    def plotGraph(self, mediaProperty=None):
         """
         Using matplotlib give estimates on all the possible media properties
         """
--- media_filesize_estimator/mediaExtraction.py
+++ media_filesize_estimator/mediaExtraction.py
@@ -1,9 +1,8 @@
 # import packages which are necessary
 import errno
-from fileinput import filename
-from msilib.schema import Error
-import os
 import mimetypes
+import os
+from fileinput import filename
 
 import pymediainfo
 
@@ -13,75 +12,81 @@ class Extraction:
     Using a media file as input, extract all the metadata in the required format
     """
 
-    def __init__(self, location = None):
+    def __init__(self, location=None):
         """
         param: requires the location of the media file
         """
         if location == None:
-            raise Exception("File not provided. Please provide a media file to perform extraction on")
-        location = r"{}".format(location)
+            raise Exception(
+                "File not provided. Please provide a media file to perform extraction on"
+            )
+        location = rf"{location}"
         if os.path.exists(location):
             self.mediaFilePath = location
         else:
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), location)
-        
+
         self.fileNameWithExtension = os.path.basename(self.mediaFilePath)
-        self.fileName = (self.fileNameWithExtension).split('.')[0]
+        self.fileName = (self.fileNameWithExtension).split(".")[0]
         mimetypes.init()
         mimestart = mimetypes.guess_type(self.fileNameWithExtension)[0]
-        
+
         if mimestart != None:
-            mimestart = mimestart.split('/')[0]
-            if mimestart in ['video','audio','image']:
-                print("The given media file is of type : {}".format(mimestart))
+            mimestart = mimestart.split("/")[0]
+            if mimestart in ["video", "audio", "image"]:
+                print(f"The given media file is of type : {mimestart}")
                 self.mediaType = mimestart
             else:
-                raise Exception("The provided file is not a media file. Please provide a valid media file")
+                raise Exception(
+                    "The provided file is not a media file. Please provide a valid media file"
+                )
         else:
-            raise Exception("Unable to identify the file type, please try with Video, Audio or Image files only")
+            raise Exception(
+                "Unable to identify the file type, please try with Video, Audio or Image files only"
+            )
 
         pass
 
-    def jsonCreation(self, extractionPath = None):
+    def jsonCreation(self, extractionPath=None):
         """
         Create a JSON file in the required location or else on the default location
         """
-        print("Extracting data from file : {}".format(self.fileNameWithExtension))
+        print(f"Extracting data from file : {self.fileNameWithExtension}")
         try:
-            jsonData = pymediainfo.MediaInfo.parse(self.mediaFilePath, output='JSON')
+            jsonData = pymediainfo.MediaInfo.parse(self.mediaFilePath, output="JSON")
         except:
             print("An error occured, please check the file and try again")
-        
-        extractionType = '.json'
-        return(self.fileCreation(jsonData, extractionPath, extractionType))
 
-    def XMLCreation(self, extractionPath = None):
+        extractionType = ".json"
+        return self.fileCreation(jsonData, extractionPath, extractionType)
+
+    def XMLCreation(self, extractionPath=None):
         """
         Create an XML file in the required location or else on a default location
         """
-        print("Extracting data from file : {}".format(self.fileNameWithExtension))
+        print(f"Extracting data from file : {self.fileNameWithExtension}")
         try:
-            XMLData = pymediainfo.MediaInfo.parse(self.mediaFilePath, output='XML')
+            XMLData = pymediainfo.MediaInfo.parse(self.mediaFilePath, output="XML")
         except:
             print("An error occured, please check the file and try again")
-        
-        extractionType = '.xml'
-        return(self.fileCreation(XMLData, extractionPath, extractionType))
 
-    def CSVCreation(self, extractionPath = None):
+        extractionType = ".xml"
+        return self.fileCreation(XMLData, extractionPath, extractionType)
+
+    def CSVCreation(self, extractionPath=None):
         """
         Create a CSV file in the required location or else on the default location
         """
-        print("Extracting data from file : {}".format(self.fileNameWithExtension))
+        print(f"Extracting data from file : {self.fileNameWithExtension}")
         try:
             mediainfoObject = pymediainfo.MediaInfo.parse(self.mediaFilePath)
             mediainfoDictionary = mediainfoObject.to_data()
         except:
             print("An error occured, please check the file and try again")
-        
-        tracksData = mediainfoDictionary['tracks']
-        extractionType = '.csv'
-        return(self.fileCreation(tracksData, extractionPath, extractionType))
+
+        tracksData = mediainfoDictionary["tracks"]
+        extractionType = ".csv"
+        return self.fileCreation(tracksData, extractionPath, extractionType)
 
     def fileCreation(self, data, extractionPath, extractionType):
         """
@@ -89,26 +94,30 @@ class Extraction:
         """
         filename = self.fileName
         if extractionPath == None:
-            print("Extraction location for the file not found, Extracting in the current working directory")
+            print(
+                "Extraction location for the file not found, Extracting in the current working directory"
+            )
             extractionPath = os.getcwd()
-        if not(os.path.exists(extractionPath)):
-            raise Exception("The directory to extract the file does not exist, please verify and try again")
-        
+        if not (os.path.exists(extractionPath)):
+            raise Exception(
+                "The directory to extract the file does not exist, please verify and try again"
+            )
+
         filenameWithExtension = filename + extractionType
         finalFile = os.path.join(extractionPath, filenameWithExtension)
-        
-        if extractionType == '.csv':
+
+        if extractionType == ".csv":
             if os.path.exists(finalFile):
                 print("File already created overwriting the content")
-            with open(finalFile, 'w+') as f:
+            with open(finalFile, "w+") as f:
                 for track in data:
                     for key in track.keys():
-                        f.write("%s, %s\n" % (key, track[key]))
+                        f.write(f"{key}, {track[key]}\n")
         else:
             if os.path.exists(finalFile):
                 print("File already created overwriting the content")
-            with open(finalFile, 'w') as f:
+            with open(finalFile, "w") as f:
                 f.write(data)
-        returnStatment = "Extraction completed successfully to file : {}".format(finalFile)
+        returnStatment = f"Extraction completed successfully to file : {finalFile}"
         print(returnStatment)
         return returnStatment
