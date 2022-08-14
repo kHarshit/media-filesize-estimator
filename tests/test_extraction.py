c5d6efca2393755a0e6fe89900fd6ad29fadfb2b
Harshit Kumar <kumar_harshit@outlook.com>
2022-08-14 12:37:49 +0530

    feat: Add tests for Extraction class

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -1,6 +1,8 @@
 """Tests for Extraction class"""
 import json
 
+from lxml import etree
+
 from media_filesize_estimator.mediaExtraction import Extraction
 
 
@@ -14,32 +16,37 @@ def ordered(obj):
         return obj
 
 
-def test_dummy():
-    "Dummy test"
-    assert 1 == 1
+def test_json():
+    """Test JSON data creation"""
+    extractionObj = Extraction("assets/sample_video_redfort.mp4")
+    extractionObj.jsonCreation()  # creates in current folder
+    with open("assets/sample_video_redfort.json") as fa, open(
+        "sample_video_redfort.json"
+    ) as fb:
+        data_a = json.load(fa)
+        data_b = json.load(fb)
+        assert ordered(data_a) == ordered(data_b)
 
 
-# def test_json():
-#     """Test JSON data creation"""
-#     extractionObj = Extraction("assets/sample_video_redfort.mp4")
-#     extractionObj.jsonCreation("assets/test_metadata.json")
-#     with open("assets/sample_video_redfort.json") as fa, open(
-#         "assets/test_metadata.json"
-#     ) as fb:
-#         data_a = json.load(fa)
-#         data_b = json.load(fb)
-#         assert ordered(data_a) == ordered(data_b)
+def test_xml():
+    """Test XML data creation"""
+    extractionObj = Extraction("assets/sample_video_redfort.mp4")
+    extractionObj.XMLCreation()  # creates in current folder
+
+    treea = etree.parse("assets/sample_video_redfort.xml")
+    treeb = etree.parse("sample_video_redfort.xml")
+    assert set(treea.getroot().itertext()) == set(treeb.getroot().itertext())
 
 
-# def test_xml():
-#     """Test XML data creation"""
-#     extractionObj = Extraction("assets/sample_video_redfort.mp4")
-#     extractionObj.jsonCreation("assets/test_metadata.xml")
-#     pass
+def test_csv():
+    """Test CSV data creation"""
+    extractionObj = Extraction("assets/sample_video_redfort.mp4")
+    extractionObj.CSVCreation()  # creates in current folder
 
-
-# def test_csv():
-#     """Test CSV data creation"""
-#     extractionObj = Extraction("assets/sample_video_redfort.mp4")
-#     extractionObj.jsonCreation("assets/test_metadata.csv")
-#     pass
+    with open("assets/sample_video_redfort.csv") as fa, open(
+        "sample_video_redfort.csv"
+    ) as fb:
+        fileone = fa.readlines()
+        filetwo = fb.readlines()
+        for line in filetwo:
+            assert line in fileone
