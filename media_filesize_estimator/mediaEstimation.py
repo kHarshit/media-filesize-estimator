33d82f1bef1a7d4c262fc679a0394f461720e138
Pappuru-Dinesh <36697509+Pappuru-Dinesh@users.noreply.github.com>
2022-08-14 13:47:49 +0530

    Updating estimate to return dictionary
    Updating estimate and plot graph to receive and send dictionary values

--- media_filesize_estimator/mediaEstimation.py
+++ media_filesize_estimator/mediaEstimation.py
@@ -3,8 +3,8 @@ from fileinput import filename
 from math import floor
 
 import pymediainfo
-
-from media_filesize_estimator.mediaExtraction import Extraction
+import matplotlib.pyplot as plt
+from mediaExtraction import Extraction
 
 
 class Estimation:
@@ -34,6 +34,7 @@ class Estimation:
             (1280, 720),
             (640, 360),
         ]
+        self.frameRateSamples = [24,30,60,90]
         pass
 
     def estimate(self, mediaProperty=None):
@@ -41,6 +42,7 @@ class Estimation:
         param : Particular media property which needs to be estimated against
         return : Estimation sizes of the media file
         """
+        plotDetails = {}
         if mediaProperty == None:
             raise Exception("Please provide a media property to provide an estimation")
         mediaType = str(self.extractionObj.mediaType)
@@ -55,34 +57,80 @@ class Estimation:
             )
 
         if mediaType == "video":
+            video_width = int(mediaData["width"])  # type: ignore
+            video_height = int(mediaData["height"])  # type: ignore
+            video_bit_depth = int(mediaData["bit_depth"])  # type: ignore
+            number_of_frames = int(mediaData["frame_count"])  # type: ignore
+            frame_rate = float(mediaData["frame_rate"])  # type: ignore
+            duration_in_seconds = floor(number_of_frames/frame_rate) # type: ignore
+            
             if mediaProperty == "resolution":
-                video_width = int(mediaData["width"])  # type: ignore
-                video_height = int(mediaData["height"])  # type: ignore
-                video_bit_depth = int(mediaData["bit_depth"])  # type: ignore
-                number_of_frames = int(mediaData["frame_count"])  # type: ignore
-                video_size = self.calculateSize(
+                video_size = floor(self.calculateSize(
                     video_width, video_height, video_bit_depth, number_of_frames
-                )
+                ))
+                key_string = str(video_width) +"x"+ str(video_height)
+                plotDetails[key_string]=video_size
                 print(
-                    f"Uncompressed file size estimation of the given file in MB with resolution {video_width} x {video_height} : {floor(video_size)}"
+                    f"Uncompressed file size estimation of the given file in MB with resolution {video_width} x {video_height} : {video_size}"
                 )
                 resolutionList = self.resolutionSamples
                 for i in resolutionList:
-                    video_size = self.calculateSize(
+                    video_size = floor(self.calculateSize(
                         i[0], i[1], video_bit_depth, number_of_frames
-                    )
+                    ))
+                    key_string = str(i[0]) +"x"+ str(i[1])
+                    plotDetails[key_string]=video_size
                     print(
-                        f"Uncompressed file size estimation of the given file in MB with resolution {i[0]} x {i[1]} : {floor(video_size)}"
+                        f"Uncompressed file size estimation of the given file in MB with resolution {i[0]} x {i[1]} : {video_size}"
+                    )
+            
+            elif mediaProperty == "frame_rate":
+                new_number_of_frames = duration_in_seconds * frame_rate
+                video_size = floor(self.calculateSize(
+                    video_width, video_height, video_bit_depth, new_number_of_frames
+                ))
+                key_string = str(frame_rate) +" FPS"
+                plotDetails[key_string]=video_size
+                print(
+                    f"Uncompressed file size estimation of the given file in MB with frame rate {frame_rate} FPS : {video_size}"
+                )
+                frame_list = self.frameRateSamples
+                for i in frame_list:
+                    frame_rate = i
+                    new_number_of_frames = duration_in_seconds * frame_rate
+                    video_size = floor(self.calculateSize(
+                        video_width, video_height, video_bit_depth, new_number_of_frames
+                    ))
+                    key_string = str(frame_rate) +" FPS"
+                    plotDetails[key_string]=video_size
+                    print(
+                        f"Uncompressed file size estimation of the given file in MB with frame rate {frame_rate} FPS : {video_size}"
                     )
 
-        # print(mediaTypeData)
-
-        pass
+        print(plotDetails)
+        return plotDetails
 
     def plotGraph(self, mediaProperty=None):
         """
         Using matplotlib give estimates on all the possible media properties
         """
+        plotDetails = self.estimate(mediaProperty)
+        print("Plotting the graph for the given parameter")
+        
+        x = [i+1 for i in range(len(plotDetails))]
+        x_label = [i for i in plotDetails.keys()]
+        y = [i for i in plotDetails.values()]
+        
+        plt.bar(x, y, tick_label=x_label)
+        if mediaProperty == 'resolution':
+            plt.xlabel("Resolutions")
+        elif mediaProperty == 'frame_rate':
+            plt.xlabel("Frame Rates")
+        
+        plt.ylabel("File Size in Mega Bytes(MB)")
+        plt.title(f"File size estimation of : {mediaProperty}")
+        plt.show()
+
         pass
 
     def calculateSize(
@@ -103,4 +151,4 @@ class Estimation:
 
 if __name__ == "__main__":
     x = Estimation(r"C:\Users\drdin\Downloads\project_test.mkv")
-    y = x.estimate("resolution")
+    y = x.plotGraph("frame_rate")
