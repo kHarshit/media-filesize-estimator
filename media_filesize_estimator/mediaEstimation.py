5137bf34f09e495662b505affb8563bdf5e86121
Harshit Kumar <kumar_harshit@outlook.com>
2022-08-14 19:24:03 +0530

    refractor: Format code

--- media_filesize_estimator/mediaEstimation.py
+++ media_filesize_estimator/mediaEstimation.py
@@ -2,8 +2,8 @@
 from fileinput import filename
 from math import floor
 
-import pymediainfo
 import matplotlib.pyplot as plt
+import pymediainfo
 from mediaExtraction import Extraction
 
 
@@ -34,10 +34,10 @@ class Estimation:
             (1280, 720),
             (640, 360),
         ]
-        self.frameRateSamples = [24,30,60,90]
-        self.bitDepths = [8,10,12,16]
-        self.sampleRates = [8,16,44.1,48]
-        self.channels = [1,2,4,6,8]
+        self.frameRateSamples = [24, 30, 60, 90]
+        self.bitDepths = [8, 10, 12, 16]
+        self.sampleRates = [8, 16, 44.1, 48]
+        self.channels = [1, 2, 4, 6, 8]
         pass
 
     def estimate(self, mediaProperty=None):
@@ -65,14 +65,14 @@ class Estimation:
             video_bit_depth = int(mediaData["bit_depth"])  # type: ignore
             number_of_frames = int(mediaData["frame_count"])  # type: ignore
             frame_rate = float(mediaData["frame_rate"])  # type: ignore
-            duration_in_seconds = floor(number_of_frames/frame_rate) # type: ignore
-            
+            duration_in_seconds = floor(number_of_frames / frame_rate)  # type: ignore
+
             if mediaProperty == "resolution":
                 video_size = self.calculateVideoSize(
                     video_width, video_height, video_bit_depth, number_of_frames
                 )
-                key_string = str(video_width) +"x"+ str(video_height)
-                plotDetails[key_string]=video_size
+                key_string = str(video_width) + "x" + str(video_height)
+                plotDetails[key_string] = video_size
                 print(
                     f"Uncompressed file size estimation of the given file in MB with resolution {key_string} : {video_size} MB"
                 )
@@ -81,19 +81,19 @@ class Estimation:
                     video_size = self.calculateVideoSize(
                         i[0], i[1], video_bit_depth, number_of_frames
                     )
-                    key_string = str(i[0]) +"x"+ str(i[1])
-                    plotDetails[key_string]=video_size
+                    key_string = str(i[0]) + "x" + str(i[1])
+                    plotDetails[key_string] = video_size
                     print(
                         f"Uncompressed file size estimation of the given file in MB with resolution {key_string} : {video_size} MB"
                     )
-            
+
             elif mediaProperty == "frame_rate":
                 new_number_of_frames = duration_in_seconds * frame_rate
                 video_size = self.calculateVideoSize(
                     video_width, video_height, video_bit_depth, new_number_of_frames
                 )
-                key_string = str(frame_rate) +" FPS"
-                plotDetails[key_string]=video_size
+                key_string = str(frame_rate) + " FPS"
+                plotDetails[key_string] = video_size
                 print(
                     f"Uncompressed file size estimation of the given file in MB with frame rate {key_string} : {video_size} MB"
                 )
@@ -104,18 +104,18 @@ class Estimation:
                     video_size = self.calculateVideoSize(
                         video_width, video_height, video_bit_depth, new_number_of_frames
                     )
-                    key_string = str(frame_rate) +" FPS"
-                    plotDetails[key_string]=video_size
+                    key_string = str(frame_rate) + " FPS"
+                    plotDetails[key_string] = video_size
                     print(
                         f"Uncompressed file size estimation of the given file in MB with frame rate {key_string} : {video_size} MB"
                     )
-            
+
             elif mediaProperty == "bit_depth":
                 video_size = self.calculateVideoSize(
                     video_width, video_height, video_bit_depth, number_of_frames
                 )
-                key_string = str(video_bit_depth) +" bit"
-                plotDetails[key_string]=video_size
+                key_string = str(video_bit_depth) + " bit"
+                plotDetails[key_string] = video_size
                 print(
                     f"Uncompressed file size estimation of the given file in MB with bit depth {key_string} : {video_size} MB"
                 )
@@ -125,42 +125,47 @@ class Estimation:
                     video_size = self.calculateVideoSize(
                         video_width, video_height, new_video_bit_depth, number_of_frames
                     )
-                    key_string = str(new_video_bit_depth) +" bit"
-                    plotDetails[key_string]=video_size
+                    key_string = str(new_video_bit_depth) + " bit"
+                    plotDetails[key_string] = video_size
                     print(
                         f"Uncompressed file size estimation of the given file in MB with bit depth {key_string} : {video_size} MB"
                     )
             else:
-                raise Exception(f"Please test with a different property as the given video file does not estimate property : {mediaProperty}")
-        
+                raise Exception(
+                    f"Please test with a different property as the given video file does not estimate property : {mediaProperty}"
+                )
+
         elif mediaType == "audio":
             audio_sampling_rate = int(mediaData["sampling_rate"])
-            audio_channels = int(mediaData['channel_s'])
+            audio_channels = int(mediaData["channel_s"])
             try:
                 audio_bit_depth = int(mediaData["bit_depth"])
             except:
                 # compressed files might or might not have bit depth
                 # if bit depth is not mentioned we are assuming it as 8 by default
                 audio_bit_depth = 8
-            audio_duration = round(int(mediaData["duration"])/1000)
+            audio_duration = round(int(mediaData["duration"]) / 1000)
 
             if mediaProperty == "sampling_rate":
                 audio_size = self.calculateAudioSize(
                     audio_sampling_rate, audio_bit_depth, audio_channels, audio_duration
                 )
-                key_string = str(round(audio_sampling_rate/1000,1)) +" kHz"
-                plotDetails[key_string]=audio_size
+                key_string = str(round(audio_sampling_rate / 1000, 1)) + " kHz"
+                plotDetails[key_string] = audio_size
                 print(
                     f"Uncompressed file size estimation of the given file in MB with sample rate {key_string} : {audio_size} MB"
                 )
-                sampleRateList = [i*1000 for i in self.sampleRates]
+                sampleRateList = [i * 1000 for i in self.sampleRates]
                 for i in sampleRateList:
                     audio_sampling_rate = i
                     audio_size = self.calculateAudioSize(
-                        audio_sampling_rate, audio_bit_depth, audio_channels, audio_duration
+                        audio_sampling_rate,
+                        audio_bit_depth,
+                        audio_channels,
+                        audio_duration,
                     )
-                    key_string = str(round(audio_sampling_rate/1000,1)) +" kHz"
-                    plotDetails[key_string]=audio_size
+                    key_string = str(round(audio_sampling_rate / 1000, 1)) + " kHz"
+                    plotDetails[key_string] = audio_size
                     print(
                         f"Uncompressed file size estimation of the given file in MB with sample rate {key_string} : {audio_size} MB"
                     )
@@ -168,8 +173,8 @@ class Estimation:
                 audio_size = self.calculateAudioSize(
                     audio_sampling_rate, audio_bit_depth, audio_channels, audio_duration
                 )
-                key_string = str(audio_bit_depth) +" bit"
-                plotDetails[key_string]=audio_size
+                key_string = str(audio_bit_depth) + " bit"
+                plotDetails[key_string] = audio_size
                 print(
                     f"Uncompressed file size estimation of the given file in MB with bit depth {key_string} : {audio_size} MB"
                 )
@@ -177,10 +182,13 @@ class Estimation:
                 for i in bit_depth_list:
                     audio_bit_depth = i
                     audio_size = self.calculateAudioSize(
-                        audio_sampling_rate, audio_bit_depth, audio_channels, audio_duration
+                        audio_sampling_rate,
+                        audio_bit_depth,
+                        audio_channels,
+                        audio_duration,
                     )
-                    key_string = str(audio_bit_depth) +" bit"
-                    plotDetails[key_string]=audio_size
+                    key_string = str(audio_bit_depth) + " bit"
+                    plotDetails[key_string] = audio_size
                     print(
                         f"Uncompressed file size estimation of the given file in MB with bit depth {key_string} : {audio_size} MB"
                     )
@@ -188,8 +196,8 @@ class Estimation:
                 audio_size = self.calculateAudioSize(
                     audio_sampling_rate, audio_bit_depth, audio_channels, audio_duration
                 )
-                key_string = str(audio_channels) +" channel"
-                plotDetails[key_string]=audio_size
+                key_string = str(audio_channels) + " channel"
+                plotDetails[key_string] = audio_size
                 print(
                     f"Uncompressed file size estimation of the given file in MB with bit depth {key_string} : {audio_size} MB"
                 )
@@ -197,15 +205,20 @@ class Estimation:
                 for i in audio_channel_list:
                     audio_channels = i
                     audio_size = self.calculateAudioSize(
-                        audio_sampling_rate, audio_bit_depth, audio_channels, audio_duration
+                        audio_sampling_rate,
+                        audio_bit_depth,
+                        audio_channels,
+                        audio_duration,
                     )
-                    key_string = str(audio_channels) +" channel"
-                    plotDetails[key_string]=audio_size
+                    key_string = str(audio_channels) + " channel"
+                    plotDetails[key_string] = audio_size
                     print(
                         f"Uncompressed file size estimation of the given file in MB with bit depth {key_string} : {audio_size} MB"
                     )
             else:
-                raise Exception(f"Please test with a different property as the given audio file does not estimate property : {mediaProperty}")
+                raise Exception(
+                    f"Please test with a different property as the given audio file does not estimate property : {mediaProperty}"
+                )
 
         return plotDetails
 
@@ -215,24 +228,24 @@ class Estimation:
         """
         plotDetails = self.estimate(mediaProperty)
         print(f"Plotting the graph for the given parameter : {mediaProperty}")
-        
-        x = [i+1 for i in range(len(plotDetails))]
+
+        x = [i + 1 for i in range(len(plotDetails))]
         x_label = [i for i in plotDetails.keys()]
         y = [i for i in plotDetails.values()]
-        colors = ["red" if i==0 else "blue" for i in range(len(x_label))]
+        colors = ["red" if i == 0 else "blue" for i in range(len(x_label))]
 
         plt.bar(x, y, tick_label=x_label, color=colors)
-        if mediaProperty == 'resolution':
+        if mediaProperty == "resolution":
             plt.xlabel("Resolutions of the Video")
-        elif mediaProperty == 'frame_rate':
+        elif mediaProperty == "frame_rate":
             plt.xlabel("Frame Rates of the Video")
-        elif mediaProperty == 'bit_depth':
+        elif mediaProperty == "bit_depth":
             plt.xlabel("Bit Depths")
-        elif mediaProperty == 'sampling_rate':
+        elif mediaProperty == "sampling_rate":
             plt.xlabel("Sample Rates of the Audio")
-        elif mediaProperty == 'channels':
+        elif mediaProperty == "channels":
             plt.xlabel("Number of Audio Channels")
-        
+
         plt.ylabel("File Size in Mega Bytes(MB)")
         plt.title(f"File size estimation of : {mediaProperty}")
         plt.show()
@@ -250,9 +263,9 @@ class Estimation:
         Calculate file size based on the parameters
         """
         size_of_frame = (video_width * video_height * video_bit_depth) / 8
-        file_size = round(((size_of_frame * number_of_frames) / (1024 * 1024)),3)
+        file_size = round(((size_of_frame * number_of_frames) / (1024 * 1024)), 3)
         return file_size
-    
+
     def calculateAudioSize(
         self,
         audio_sampling_rate=None,
@@ -263,8 +276,10 @@ class Estimation:
         """
         Calculate file size based on the parameters
         """
-        file_size_bytes = (audio_sampling_rate * audio_bit_depth * audio_channels * audio_duration) / 8
-        file_size = round(file_size_bytes/(1024*1024),3)
+        file_size_bytes = (
+            audio_sampling_rate * audio_bit_depth * audio_channels * audio_duration
+        ) / 8
+        file_size = round(file_size_bytes / (1024 * 1024), 3)
         return file_size
 
 
